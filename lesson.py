dea41d7a94d99229f7ee76bf19ec5a8b194ee951
IsmoilObidov <ismoilobidov10001@gmail.com>
2021-06-23 23:17:45 +0300

    Update lesson.py

---
+++ lesson.py
@@ -0,0 +1,66 @@
+import math
+# a = int(input("a = "))
+
+
+# d = a % 2
+
+# if (d == 0) :
+#     print("juft")
+# else:
+#     print("toq")
+
+
+
+
+# a = int(input("a = "))
+# if a > 0 :
+#     print('musbat')
+# else :
+#     print('manfiy')
+
+
+
+
+
+
+
+# a<=b<=c
+
+# a = int(input('a = '))
+# b = int(input('b = '))
+# c = int(input('c = '))
+
+# if (a>b):
+#     print('b soni a dan katta bolsin!')
+
+# if (a>c):
+#     print('c soni a dan katta bolsin!')
+
+# if (b>c):
+#     print('c soni b dan katta bolsin!')
+
+# if (a>b<c):
+#     print('b soni a dan katta bolsin!')    
+
+# if (a<b>c):
+#     print('c soni b dan katta bolsin!')
+
+# if (a>b>c):
+#     print('c va b sonlari  dan katta bolsin!')
+
+# if(a<=b<=c):
+#     print("Ifoda to'g'ri bajarildi" , a ,"<=" ,b , "<=" ,c)
+
+
+
+
+
+
+
+a = int(input('a = '))
+b = int(input('b = '))
+c = int(input('c = '))
+
+x = max(a,b,c)
+
+print("\n" ,x)
